fd00b96807896a713bc04f93fa3b444233f41cf3
Lorde Aleister <lordealeister@gmail.com>
2021-11-11 12:42:44 -0300

    Fix autoload

--- popular-posts.php
+++ popular-posts.php
@@ -16,11 +16,16 @@ if(file_exists(dirname(__FILE__) . '/vendor/cmb2/cmb2/init.php'))
     require_once dirname(__FILE__) . '/vendor/cmb2/cmb2/init.php';
 elseif(!is_plugin_active('cmb2/init.php') && file_exists(dirname(__DIR__, 1) . '/cmb2/init.php'))
     require_once dirname(__DIR__, 1) . '/cmb2/init.php';
+elseif(!is_plugin_active('cmb2/init.php') && file_exists(dirname(__DIR__, 2) . '/cmb2/cmb2/init.php'))
+  require_once dirname(__DIR__, 2) . '/cmb2/cmb2/init.php';
+    
 
 if(file_exists(dirname(__FILE__) . '/vendor/abuyoyo/cmb2-switch-button/cmb2-switch-button.php'))
     require_once dirname(__FILE__) . '/vendor/abuyoyo/cmb2-switch-button/cmb2-switch-button.php';
 elseif(!is_plugin_active('cmb2-switch-button/cmb2-switch-button.php') && file_exists(dirname(__DIR__, 1) . '/cmb2-switch-button/cmb2-switch-button.php'))
     require_once dirname(__DIR__, 1) . '/cmb2-switch-button/cmb2-switch-button.php';
+elseif(!is_plugin_active('cmb2-switch-button/cmb2-switch-button.php') && file_exists(dirname(__DIR__, 2) . '/abuyoyo/cmb2-switch-button/cmb2-switch-button.php'))
+    require_once dirname(__DIR__, 2) . '/abuyoyo/cmb2-switch-button/cmb2-switch-button.php';
 
 if(defined('CMB2_LOADED') && class_exists('CMB2_Switch_Button'))
     new PopularPosts\App;
